874ca0c3d41c16a93afdfd066aebb516a48269a2
Kieran <Kierancoder@gmail.com>
2025-05-14 14:20:18 +0100

    test

--- main.tf
+++ main.tf
@@ -1,37 +1,26 @@
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
 }
+
+provider "docker" {}
+
+variable "image_tag" {}
 
 resource "docker_image" "app" {
-  name         = "yourdockerhubusername/devops:latest"  # Docker Hub repo name
-  build {
-    context    = "./"           # Path to your Dockerfile
-    dockerfile = "./Dockerfile" # Path to your Dockerfile
-  }
-  tags = ["latest", var.image_tag]  # Tag the image with both `latest` and a dynamic image tag
+  name = "xraiinbowcoder/devops:${var.image_tag}"
 }
 
-resource "docker_registry_image" "app" {
-  name = "yourdockerhubusername/devops:latest"  # The same as the Docker image name
+resource "docker_container" "app" {
+  name  = "devops-app"
+  image = docker_image.app.name
 
-  push {
-    username = var.docker_username
-    password = var.docker_password
+  ports {
+    internal = 10049
+    external = 10049
   }
 }
-
-variable "image_tag" {
-  type    = string
-  default = "v1.0"  # Example tag (this can be dynamic based on the Git commit hash or version)
-}
-
-variable "docker_username" {
-  type    = string
-  default = "yourdockerhubusername"
-}
-
-variable "docker_password" {
-  type    = string
-  default = "yourdockerhubpassword"
-}
-
