dff59ce74446866bb74e3b1f42b5a68712649b23
xRaiinBow-Coder <kierancoder@gmail.com>
2025-05-16 10:52:20 +0100

    another test

--- index.test.js
+++ index.test.js
@@ -10,6 +10,14 @@ beforeAll(async () => {
   });
 });
 
+decscribe("GET /", () => {
+  it("should redirect to /students", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(302);
+    expect(res.headers.location).toBe("/students");
+  });
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
